1b742918dea2df0831f8bbc2663edf9fd8cfc0b0
Josh Schonstal <josh@schonstal.org>
2014-04-27 12:50:26 -0700

    More mapgen

--- source/Dungeon.hx
+++ source/Dungeon.hx
@@ -9,44 +9,53 @@ import flixel.tile.FlxTilemap;
 
 class Dungeon extends FlxGroup
 {
+  inline static var SIZE = 40;
+
   var dungeonTiles:DungeonTiles;
   var drapedTiles:DrapedTiles;
+  var isometricWalls:IsometricWalls;
 
   var groundTilemap:FlxTilemap;
   var drapedTilemap:FlxTilemap;
+  var wallTilemap:FlxTilemap;
 
   public function new() {
     super();
-    dungeonTiles = new DungeonTiles(40,40);
-    var flattenedArray:Array<Int> = new Array<Int>(); 
-    for (tileArray in dungeonTiles.tiles) {
-      for (tile in tileArray) {
-        flattenedArray.push(tile);
-      }
-    }
+    dungeonTiles = new DungeonTiles(SIZE,SIZE);
 
     groundTilemap = new FlxTilemap();
-    groundTilemap.loadMap(FlxStringUtil.arrayToCSV(flattenedArray, 40),
+    groundTilemap.loadMap(FlxStringUtil.arrayToCSV(flattenArray(dungeonTiles.tiles), SIZE),
                           "assets/images/tiles.png", 32, 32, FlxTilemap.OFF);
     groundTilemap.x = -20 * 32;
     groundTilemap.y = -20 * 32;
     add(groundTilemap);
 
-
     drapedTiles = new DrapedTiles(dungeonTiles);
-    flattenedArray = new Array<Int>(); 
-    for (tileArray in drapedTiles.tiles) {
-      for (tile in tileArray) {
-        flattenedArray.push(tile);
-      }
-    }
 
     drapedTilemap = new FlxTilemap();
-    drapedTilemap.loadMap(FlxStringUtil.arrayToCSV(flattenedArray, 40),
+    drapedTilemap.loadMap(FlxStringUtil.arrayToCSV(flattenArray(drapedTiles.tiles), SIZE),
                           "assets/images/tiles.png", 32, 32, FlxTilemap.OFF);
     drapedTilemap.x = groundTilemap.x;
     drapedTilemap.y = groundTilemap.y;
     add(drapedTilemap);
 
+    isometricWalls = new IsometricWalls(dungeonTiles);
+    wallTilemap = new FlxTilemap();
+    wallTilemap.loadMap(FlxStringUtil.arrayToCSV(flattenArray(isometricWalls.tiles), SIZE),
+                          "assets/images/tiles.png", 32, 32, FlxTilemap.OFF);
+    wallTilemap.x = groundTilemap.x;
+    wallTilemap.y = groundTilemap.y;
+    add(wallTilemap);
+  }
+
+  private function flattenArray(array:Array<Array<Int>>):Array<Int> {
+    var flattenedArray = new Array<Int>(); 
+    for (tileArray in array) {
+      for (tile in tileArray) {
+        flattenedArray.push(tile);
+      }
+    }
+
+    return flattenedArray;
   }
 }
---
+++ source/IsometricWalls.hx
@@ -0,0 +1,99 @@
+package;
+
+import flixel.FlxObject;
+import flixel.util.FlxRandom;
+import flixel.util.FlxPoint;
+import flixel.util.FlxVector;
+
+class IsometricWalls
+{
+  inline static var NORTH = 0x01;
+  inline static var EAST  = 0x02;
+  inline static var SOUTH = 0x04;
+  inline static var WEST  = 0x08;
+
+  public var tiles:Array<Array<Int>>;
+  public var topTiles:Array<Array<Int>>;
+
+  var dungeonTiles:DungeonTiles;
+
+  /* Index on tilemap is bitwise or with presence of adjacent tiles on the cardinal directions
+   * Starting with the least significant bit:
+   * 0b0001 = 0x01 => A tile is present in the north
+   * 0b0010 = 0x02 => A tile is present in the east
+   * 0b0100 = 0x04 => A tile is present in the south
+   * 0b1000 = 0x08 => A tile is present in the west
+   */
+
+  var tileMap:Array<Array<Int>> = [
+    [16],             // -          (0b0000)
+    [16],             // N          (0b0001)
+    [16],             // E          (0b0010)
+    [16],             // N, E       (0b0011)
+    [16, 17, 18, 19], // S          (0b0100)
+    [16, 17, 18, 19], // N, S       (0b0101)
+    [16, 17, 18, 19], // S, E       (0b0110)
+    [16, 17, 18, 19], // S, E, N    (0b0111)
+    [16],             // W          (0b1000)
+    [16],             // W, N       (0b1001)
+    [16],             // W, E       (0b1010)
+    [16],             // W, E, N    (0b1011)
+    [16, 17, 18, 19], // W, S       (0b1100)
+    [16, 17, 18, 19], // W, S, N    (0b1101)
+    [16, 17, 18, 19], // W, S, E    (0b1110)
+    [16, 17, 18, 19]  // N, E, S, W (0b1111)
+  ];
+
+  public function new(dungeonTiles) {
+    this.dungeonTiles = dungeonTiles;
+    generateTiles();
+  }
+
+  private function generateTiles():Void {
+    tiles = new Array<Array<Int>>();
+    //Pass through tilemap and add solids
+    for(y in 0...dungeonTiles.height) {
+      tiles[y] = new Array<Int>();
+      for(x in 0...dungeonTiles.width) {
+        if(dungeonTiles.tiles[y][x] == 0) {
+          tiles[y][x] = 16;
+        } else {
+          tiles[y][x] = 0;
+        }
+      }
+    }
+
+    //TODO: Cut out lakes
+
+    //Find edges
+    for(y in 0...dungeonTiles.height) {
+      for(x in 0...dungeonTiles.width) {
+        if(tiles[y][x] > 0) {
+          layTile(x, y);
+        }
+      }
+    }
+  }
+
+  private function layTile(x:Int, y:Int):Void {
+    var quadIndex:Int = 0;
+    var tileIndex = 0;
+
+    if(y > 0 && tiles[y-1][x] > 0) tileIndex |= NORTH;
+    if(x < tiles[y].length-1 && tiles[y][x+1] > 0) tileIndex |= EAST;
+
+    //If there's a tile below, we have to pick from one of four tiles.
+    if(y < tiles.length-1) {
+      var row = tiles[y+1];
+      if(row[x] > 0) {
+        tileIndex |= SOUTH;
+        if(x < row.length-1 && row[x+1] > 0) quadIndex |= 0x01;
+        if(x > 0 && row[x-1] > 0) quadIndex |= 0x02;
+      }
+    }
+
+    if(x > 0 && tiles[y][x-1] > 0) tileIndex |= WEST;
+
+    tiles[y][x] = tileMap[tileIndex][quadIndex];
+  }
+}
